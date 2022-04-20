950a1d4b0ee9f67db449b5afe05c72b3b0dca1b5
mnikn <mnikn@outlook.com>
2022-04-20 22:20:54 +0800

    feat: update assets

--- Player.gd
+++ Player.gd
@@ -1,17 +1,50 @@
 extends KinematicBody2D
 
 const SPEED = 10
+const ATTACK_SPPED = 25
 
 var velocity = Vector2.ZERO
 var direction = "down" setget set_direction
+var attacking = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
+func _input(event):
+	if event.is_action_pressed("player_attack"):
+		var tween = Tween.new()
+		var new_vel = Vector2.ZERO
+		if self.direction == "down":
+			new_vel = Vector2(0, ATTACK_SPPED)
+			$AnimationPlayer.play("attack_down")
+		elif self.direction == "up":
+			new_vel = Vector2(0, -ATTACK_SPPED)
+			$AnimationPlayer.play("attack_up")
+		elif self.direction == "left":
+			new_vel = Vector2(-ATTACK_SPPED, 0)
+			$AnimationPlayer.play("attack_left")
+		else:
+			new_vel = Vector2(ATTACK_SPPED, 0)
+			$AnimationPlayer.play("attack_right")
+		tween.interpolate_property(self, "velocity", self.velocity, new_vel, 0.15, Tween.TRANS_BOUNCE)
+		self.add_child(tween)
+		tween.start()
+		self.attacking = true
+		yield(tween, "tween_all_completed")
+		yield($AnimationPlayer, "animation_finished")
+		yield(self.get_tree().create_timer(0.03), "timeout")
+		self.velocity = Vector2.ZERO
+		yield(self.get_tree().create_timer(0.07), "timeout")
+		self.attacking = false
+		tween.queue_free()
 
 func _process(delta):
+	if self.attacking:
+		self.move_and_collide(self.velocity)
+		return
+
 	if Input.get_action_strength("player_move_down") > 0:
 		if $AnimationPlayer.current_animation != "move_down":
 			$AnimationPlayer.play("move_down")
